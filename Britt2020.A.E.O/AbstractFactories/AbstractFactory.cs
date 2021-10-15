6e957e0535263929ef23788478b8665a85cf5429
JustinBritt <JustinBritt@users.noreply.github.com>
2021-10-15 08:10:36 -0400

    Added static Create method

--- Britt2020.A.E.O/AbstractFactories/AbstractFactory.cs
+++ Britt2020.A.E.O/AbstractFactories/AbstractFactory.cs
@@ -20,6 +20,11 @@
                     fileStream);
             }
         }
+        
+        public static IAbstractFactory Create()
+        {
+            return new AbstractFactory();
+        }
 
         public IConfigurationsAbstractFactory CreateConfigurationsAbstractFactory()
         {
