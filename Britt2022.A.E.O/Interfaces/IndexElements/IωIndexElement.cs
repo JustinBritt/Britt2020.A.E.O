2c8cb1e0af4e7e9b76a82dfea45dee7e47ddf7fc
Justin Britt <britt@uwindsor.ca>
2023-06-10 05:50:25 -0400

    Implement IComparable

--- Britt2022.A.E.O/Interfaces/IndexElements/IωIndexElement.cs
+++ Britt2022.A.E.O/Interfaces/IndexElements/IωIndexElement.cs
@@ -1,8 +1,10 @@
 ﻿namespace Britt2022.A.E.O.Interfaces.IndexElements
 {
+    using System;
+
     using Hl7.Fhir.Model;
 
-    public interface IωIndexElement
+    public interface IωIndexElement : IComparable<IωIndexElement>
     {
         /// <summary>
         /// Gets the scenario.
