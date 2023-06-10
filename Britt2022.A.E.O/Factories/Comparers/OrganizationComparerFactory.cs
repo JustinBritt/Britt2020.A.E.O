b1233b4ea867d66c44eb298c35066f227c6245f1
Justin Britt <britt@uwindsor.ca>
2023-06-10 06:46:48 -0400

    Added factory

---
+++ Britt2022.A.E.O/Factories/Comparers/OrganizationComparerFactory.cs
@@ -0,0 +1,37 @@
+﻿namespace Britt2022.A.E.O.Factories.Comparers
+{
+    using System;
+
+    using log4net;
+
+    using Britt2022.A.E.O.Classes.Comparers;
+    using Britt2022.A.E.O.Interfaces.Comparers;
+    using Britt2022.A.E.O.InterfacesFactories.Comparers;
+
+    internal sealed class OrganizationComparerFactory : IOrganizationComparerFactory
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public OrganizationComparerFactory()
+        {
+        }
+
+        public IOrganizationComparer Create()
+        {
+            IOrganizationComparer instance = null;
+
+            try
+            {
+                instance = new OrganizationComparer();
+            }
+            catch (Exception exception)
+            {
+                this.Log.Error(
+                    exception.Message,
+                    exception);
+            }
+
+            return instance;
+        }
+    }
+}
