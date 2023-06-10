e9e3a55d7271912de046712262a98d1ce982cfc1
Justin Britt <britt@uwindsor.ca>
2023-06-10 07:02:24 -0400

    Added abstract factory

---
+++ Britt2022.A.E.O/InterfacesAbstractFactories/IComparersAbstractFactory.cs
@@ -0,0 +1,9 @@
+﻿namespace Britt2022.A.E.O.InterfacesAbstractFactories
+{
+    using Britt2022.A.E.O.InterfacesFactories.Comparers;
+
+    public interface IComparersAbstractFactory
+    {
+        IOrganizationComparerFactory CreateOrganizationComparerFactory();
+    }
+}
