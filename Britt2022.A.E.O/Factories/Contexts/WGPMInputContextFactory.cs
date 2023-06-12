636f9e130a99f7486905714fe4aa9457a29d70ab
Justin Britt <britt@uwindsor.ca>
2023-06-12 08:20:33 -0400

    Use red black tree

--- Britt2022.A.E.O/Factories/Contexts/WGPMInputContextFactory.cs
+++ Britt2022.A.E.O/Factories/Contexts/WGPMInputContextFactory.cs
@@ -43,7 +43,7 @@
             INullableValue<decimal> goalWeight2,
             INullableValue<decimal> goalWeight3,
             INullableValue<decimal> goalWeight4,
-            ImmutableList<Tuple<Organization, Location, FhirBoolean>> surgeonOperatingRoomAvailabilities,
+            RedBlackTree<Organization, RedBlackTree<Location, INullableValue<bool>>> surgeonOperatingRoomAvailabilities,
             RedBlackTree<INullableValue<int>, INullableValue<decimal>> scenarioProbabilities,
             ImmutableList<Tuple<Organization, PositiveInt, PositiveInt, FhirDecimal>> surgeonDayScenarioCumulativeNumberPatients,
             RedBlackTree<Organization, RedBlackTree<FhirDateTime, INullableValue<bool>>> surgeonDayAvailabilities)
