26a3bd0336643395e6d05f1319766e90196b0476
Justin Britt <britt@uwindsor.ca>
2023-06-13 00:47:16 -0400

    Added visitor

---
+++ Britt2022.A.E.O/Visitors/Contexts/SurgeonScenarioWeightedAverageSurgicalDurationsInnerVisitor.cs
@@ -0,0 +1,61 @@
+﻿namespace Britt2022.A.E.O.Visitors.Contexts
+{
+    using System.Collections.Generic;
+
+    using log4net;
+
+    using Hl7.Fhir.Model;
+
+    using NGenerics.DataStructures.Trees;
+
+    using Britt2022.A.E.O.Interfaces.IndexElements;
+    using Britt2022.A.E.O.Interfaces.Indices;
+    using Britt2022.A.E.O.Interfaces.ParameterElements.Surgeries;
+    using Britt2022.A.E.O.InterfacesFactories.ParameterElements.Surgeries;
+    using Britt2022.A.E.O.InterfacesVisitors.Contexts;
+
+    internal sealed class SurgeonScenarioWeightedAverageSurgicalDurationsInnerVisitor<TKey, TValue> : ISurgeonScenarioWeightedAverageSurgicalDurationsInnerVisitor<TKey, TValue>
+        where TKey : INullableValue<int>
+        where TValue : Duration
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public SurgeonScenarioWeightedAverageSurgicalDurationsInnerVisitor(
+            IAParameterElementFactory AParameterElementFactory,
+            IiIndexElement iIndexElement,
+            Iω ω)
+        {
+            this.AParameterElementFactory = AParameterElementFactory;
+
+            this.iIndexElement = iIndexElement;
+
+            this.ω = ω;
+
+            this.RedBlackTree = new RedBlackTree<IωIndexElement, IAParameterElement>();
+        }
+
+        private IAParameterElementFactory AParameterElementFactory { get; }
+
+        private IiIndexElement iIndexElement { get; }
+
+        private Iω ω { get; }
+
+        public bool HasCompleted => false;
+
+        public RedBlackTree<IωIndexElement, IAParameterElement> RedBlackTree { get; }
+
+        public void Visit(
+            KeyValuePair<TKey, TValue> obj)
+        {
+            IωIndexElement ωIndexElement = this.ω.GetElementAt(
+                obj.Key);
+
+            this.RedBlackTree.Add(
+                ωIndexElement,
+                this.AParameterElementFactory.Create(
+                    this.iIndexElement,
+                    ωIndexElement,
+                    obj.Value));
+        }
+    }
+}
---
+++ Britt2022.A.E.O/Visitors/Contexts/SurgeonScenarioWeightedAverageSurgicalDurationsOuterVisitor.cs
@@ -0,0 +1,69 @@
+﻿namespace Britt2022.A.E.O.Visitors.Contexts
+{
+    using System.Collections.Generic;
+
+    using log4net;
+
+    using Hl7.Fhir.Model;
+
+    using NGenerics.DataStructures.Trees;
+    using NGenerics.Patterns.Visitor;
+
+    using Britt2022.A.E.O.Interfaces.IndexElements;
+    using Britt2022.A.E.O.Interfaces.Indices;
+    using Britt2022.A.E.O.Interfaces.ParameterElements.Surgeries;
+    using Britt2022.A.E.O.InterfacesFactories.ParameterElements.Surgeries;
+    using Britt2022.A.E.O.InterfacesVisitors.Contexts;
+
+    internal sealed class SurgeonScenarioWeightedAverageSurgicalDurationsOuterVisitor<TKey, TValue> : ISurgeonScenarioWeightedAverageSurgicalDurationsOuterVisitor<TKey, TValue>
+        where TKey : Organization
+        where TValue : RedBlackTree<INullableValue<int>, Duration>
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public SurgeonScenarioWeightedAverageSurgicalDurationsOuterVisitor(
+            IAParameterElementFactory AParameterElementFactory,
+            Ii i,
+            Iω ω)
+        {
+            this.AParameterElementFactory = AParameterElementFactory;
+
+            this.i = i;
+
+            this.ω = ω;
+
+            this.RedBlackTree = new RedBlackTree<IiIndexElement, RedBlackTree<IωIndexElement, IAParameterElement>>();
+        }
+
+        private IAParameterElementFactory AParameterElementFactory { get; }
+
+        private Ii i { get; }
+
+        private Iω ω { get; }
+
+        public bool HasCompleted => false;
+
+        public RedBlackTree<IiIndexElement, RedBlackTree<IωIndexElement, IAParameterElement>> RedBlackTree { get; }
+
+        public void Visit(
+            KeyValuePair<TKey, TValue> obj)
+        {
+            IiIndexElement iIndexElement = this.i.GetElementAt(
+                obj.Key);
+
+            RedBlackTree<INullableValue<int>, Duration> value = obj.Value;
+
+            var innerVisitor = new SurgeonScenarioWeightedAverageSurgicalDurationsInnerVisitor<INullableValue<int>, Duration>(
+                this.AParameterElementFactory,
+                iIndexElement,
+                this.ω);
+
+            value.AcceptVisitor(
+                innerVisitor);
+
+            this.RedBlackTree.Add(
+                iIndexElement,
+                innerVisitor.RedBlackTree);
+        }
+    }
+}
