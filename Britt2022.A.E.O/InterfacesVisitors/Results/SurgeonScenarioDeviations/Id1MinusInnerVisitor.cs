3ef7c6b9b51401f4a2f7644d046d45a3b5af67f6
Justin Britt <britt@uwindsor.ca>
2023-07-10 11:55:52 -0400

    Added visitor

---
+++ Britt2022.A.E.O/InterfacesVisitors/Results/SurgeonScenarioDeviations/Id1MinusInnerVisitor.cs
@@ -0,0 +1,19 @@
+﻿namespace Britt2022.A.E.O.InterfacesVisitors.Results.SurgeonScenarioDeviations
+{
+    using System.Collections.Generic;
+
+    using Hl7.Fhir.Model;
+
+    using NGenerics.DataStructures.Trees;
+    using NGenerics.Patterns.Visitor;
+
+    using Britt2022.A.E.O.Interfaces.IndexElements;
+    using Britt2022.A.E.O.Interfaces.ResultElements.SurgeonScenarioDeviations;
+   
+    public interface Id1MinusInnerVisitor<TKey, TValue> : IVisitor<KeyValuePair<TKey, TValue>>
+        where TKey : IωIndexElement
+        where TValue : Id1MinusResultElement
+    {
+        RedBlackTree<INullableValue<int>, INullableValue<int>> RedBlackTree { get; }
+    }
+}
