ce93e76b45b082e253a553f4732982e8124f41a3
Justin Britt <britt@uwindsor.ca>
2023-07-09 23:39:06 -0400

    Added visitor

---
+++ Britt2022.A.E.O/Visitors/Results/ScenarioRecoveryWardCensuses/IMaxVisitor.cs
@@ -0,0 +1,50 @@
+﻿namespace Britt2022.A.E.O.Visitors.Results.ScenarioRecoveryWardCensuses
+{
+    using System.Collections.Generic;
+
+    using log4net;
+
+    using Hl7.Fhir.Model;
+
+    using NGenerics.DataStructures.Trees;
+
+    using Britt2022.A.E.O.Interfaces.Comparers;
+    using Britt2022.A.E.O.Interfaces.IndexElements;
+    using Britt2022.A.E.O.Interfaces.ResultElements.ScenarioRecoveryWardCensuses;
+    using Britt2022.A.E.O.InterfacesFactories.Dependencies.NGenerics.DataStructures.Trees;
+    using Britt2022.A.E.O.InterfacesFactories.Dependencies.Hl7.Fhir.R4.Model;
+    using Britt2022.A.E.O.InterfacesVisitors.Results.ScenarioRecoveryWardCensuses;
+
+    internal sealed class IMaxVisitor<TKey, TValue> : IIMaxVisitor<TKey, TValue>
+        where TKey : IωIndexElement
+        where TValue : IIMaxResultElement
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public IMaxVisitor(
+            INullableValueFactory nullableValueFactory,
+            IRedBlackTreeFactory redBlackTreeFactory,
+            INullableValueintComparer nullableValueintComparer)
+        {
+            this.NullableValueFactory = nullableValueFactory;
+
+            this.RedBlackTree = redBlackTreeFactory.Create<INullableValue<int>, INullableValue<decimal>>(
+                nullableValueintComparer);
+        }
+
+        private INullableValueFactory NullableValueFactory { get; }
+
+        public bool HasCompleted => false;
+
+        public RedBlackTree<INullableValue<int>, INullableValue<decimal>> RedBlackTree { get; }
+
+        public void Visit(
+            KeyValuePair<TKey, TValue> obj)
+        {
+            this.RedBlackTree.Add(
+                obj.Key.Value,
+                this.NullableValueFactory.Create<decimal>(
+                    obj.Value.Value));
+        }
+    }
+}
