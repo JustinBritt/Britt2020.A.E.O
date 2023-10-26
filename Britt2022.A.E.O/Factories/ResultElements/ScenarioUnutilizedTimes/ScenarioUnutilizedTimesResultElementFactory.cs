b8d7425eef0afae16f1dfb94e0c645247c69a2e7
Justin Britt <britt@uwindsor.ca>
2023-10-26 08:09:09 -0400

    Added factory

---
+++ Britt2022.A.E.O/Factories/ResultElements/ScenarioUnutilizedTimes/ScenarioUnutilizedTimesResultElementFactory.cs
@@ -0,0 +1,42 @@
+﻿namespace Britt2022.A.E.O.Factories.ResultElements.ScenarioUnutilizedTimes
+{
+    using System;
+
+    using log4net;
+
+    using Britt2022.A.E.O.Classes.ResultElements.ScenarioUnutilizedTimes;
+    using Britt2022.A.E.O.Interfaces.IndexElements;
+    using Britt2022.A.E.O.Interfaces.ResultElements.ScenarioUnutilizedTimes;
+    using Britt2022.A.E.O.InterfacesFactories.ResultElements.ScenarioUnutilizedTimes;
+
+    internal sealed class ScenarioUnutilizedTimesResultElementFactory : IScenarioUnutilizedTimesResultElementFactory
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public ScenarioUnutilizedTimesResultElementFactory()
+        {
+        }
+
+        public IScenarioUnutilizedTimesResultElement Create(
+            IωIndexElement ωIndexElement,
+            decimal value)
+        {
+            IScenarioUnutilizedTimesResultElement resultElement = null;
+
+            try
+            {
+                resultElement = new ScenarioUnutilizedTimesResultElement(
+                    ωIndexElement,
+                    value);
+            }
+            catch (Exception exception)
+            {
+                this.Log.Error(
+                    exception.Message,
+                    exception);
+            }
+
+            return resultElement;
+        }
+    }
+}
---
+++ Britt2022.A.E.O/InterfacesFactories/ResultElements/ScenarioUnutilizedTimes/IScenarioUnutilizedTimesResultElementFactory.cs
@@ -0,0 +1,12 @@
+﻿namespace Britt2022.A.E.O.InterfacesFactories.ResultElements.ScenarioUnutilizedTimes
+{
+    using Britt2022.A.E.O.Interfaces.IndexElements;
+    using Britt2022.A.E.O.Interfaces.ResultElements.ScenarioUnutilizedTimes;
+
+    public interface IScenarioUnutilizedTimesResultElementFactory
+    {
+        IScenarioUnutilizedTimesResultElement Create(
+            IωIndexElement ωIndexElement,
+            decimal value);
+    }
+}
