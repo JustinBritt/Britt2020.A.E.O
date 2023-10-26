d4f3edee5b1063f43a4cd97b60bd0e05215d69d2
Justin Britt <britt@uwindsor.ca>
2023-10-26 06:26:18 -0400

    Added class

---
+++ Britt2022.A.E.O/Classes/ResultElements/ScenarioTotalTimes/ScenarioTotalTimesResultElement.cs
@@ -0,0 +1,25 @@
+﻿namespace Britt2022.A.E.O.Classes.ResultElements.ScenarioTotalTimes
+{
+    using log4net;
+
+    using Britt2022.A.E.O.Interfaces.IndexElements;
+    using Britt2022.A.E.O.Interfaces.ResultElements.ScenarioTotalTimes;
+
+    internal sealed class ScenarioTotalTimesResultElement : IScenarioTotalTimesResultElement
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public ScenarioTotalTimesResultElement(
+            IωIndexElement ωIndexElement,
+            decimal value)
+        {
+            this.ωIndexElement = ωIndexElement;
+
+            this.Value = value;
+        }
+
+        public IωIndexElement ωIndexElement { get; }
+
+        public decimal Value { get; }
+    }
+}
---
+++ Britt2022.A.E.O/Interfaces/ResultElements/ScenarioTotalTimes/IScenarioTotalTimesResultElement.cs
@@ -0,0 +1,11 @@
+﻿namespace Britt2022.A.E.O.Interfaces.ResultElements.ScenarioTotalTimes
+{
+    using Britt2022.A.E.O.Interfaces.IndexElements;
+
+    public interface IScenarioTotalTimesResultElement
+    {
+        IωIndexElement ωIndexElement { get; }
+
+        decimal Value { get; }
+    }
+}
