5de2a49b5656fb0878c4ee5e2cc53784064478a8
Justin Britt <britt@uwindsor.ca>
2023-10-26 09:15:26 -0400

    Added factory

---
+++ Britt2022.A.E.O/Factories/ResultElements/ScenarioUnderutilizations/ScenarioUnderutilizationsResultElementFactory.cs
@@ -0,0 +1,42 @@
+﻿namespace Britt2022.A.E.O.Factories.ResultElements.ScenarioUnderutilizations
+{
+    using System;
+
+    using log4net;
+
+    using Britt2022.A.E.O.Classes.ResultElements.ScenarioUnderutilizations;
+    using Britt2022.A.E.O.Interfaces.IndexElements;
+    using Britt2022.A.E.O.Interfaces.ResultElements.ScenarioUnderutilizations;
+    using Britt2022.A.E.O.InterfacesFactories.ResultElements.ScenarioUnderutilizations;
+
+    internal sealed class ScenarioUnderutilizationsResultElementFactory : IScenarioUnderutilizationsResultElementFactory
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public ScenarioUnderutilizationsResultElementFactory()
+        {
+        }
+
+        public IScenarioUnderutilizationsResultElement Create(
+            IωIndexElement ωIndexElement,
+            decimal value)
+        {
+            IScenarioUnderutilizationsResultElement instance = null;
+
+            try
+            {
+                instance = new ScenarioUnderutilizationsResultElement(
+                    ωIndexElement,
+                    value);
+            }
+            catch (Exception exception)
+            {
+                this.Log.Error(
+                    exception.Message,
+                    exception);
+            }
+
+            return instance;
+        }
+    }
+}
---
+++ Britt2022.A.E.O/InterfacesFactories/ResultElements/ScenarioUnderutilizations/IScenarioUnderutilizationsResultElementFactory.cs
@@ -0,0 +1,12 @@
+﻿namespace Britt2022.A.E.O.InterfacesFactories.ResultElements.ScenarioUnderutilizations
+{
+    using Britt2022.A.E.O.Interfaces.IndexElements;
+    using Britt2022.A.E.O.Interfaces.ResultElements.ScenarioUnderutilizations;
+
+    public interface IScenarioUnderutilizationsResultElementFactory
+    {
+        IScenarioUnderutilizationsResultElement Create(
+            IωIndexElement ωIndexElement,
+            decimal value);
+    }
+}
