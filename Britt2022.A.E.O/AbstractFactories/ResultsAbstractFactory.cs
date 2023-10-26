0cd103bd68ebb53350be0e7166c7f8f75be1da8f
Justin Britt <britt@uwindsor.ca>
2023-10-26 06:43:59 -0400

    Changes to usings

--- Britt2022.A.E.O/AbstractFactories/ResultsAbstractFactory.cs
+++ Britt2022.A.E.O/AbstractFactories/ResultsAbstractFactory.cs
@@ -11,6 +11,7 @@
     using Britt2022.A.E.O.Factories.Results.ObjectiveValue;
     using Britt2022.A.E.O.Factories.Results.OverallWallTime;
     using Britt2022.A.E.O.Factories.Results.ScenarioRecoveryWardCensuses;
+    using Britt2022.A.E.O.Factories.Results.ScenarioTotalTimes;
     using Britt2022.A.E.O.Factories.Results.SurgeonOperatingRoomDayAssignments;
     using Britt2022.A.E.O.Factories.Results.SurgeonOperatingRoomDayScenarioDeviations;
     using Britt2022.A.E.O.Factories.Results.SurgeonScenarioDeviations;
@@ -22,6 +23,7 @@
     using Britt2022.A.E.O.InterfacesFactories.Results.ObjectiveValue;
     using Britt2022.A.E.O.InterfacesFactories.Results.OverallWallTime;
     using Britt2022.A.E.O.InterfacesFactories.Results.ScenarioRecoveryWardCensuses;
+    using Britt2022.A.E.O.InterfacesFactories.Results.ScenarioTotalTimes;
     using Britt2022.A.E.O.InterfacesFactories.Results.SurgeonOperatingRoomDayAssignments;
     using Britt2022.A.E.O.InterfacesFactories.Results.SurgeonOperatingRoomDayScenarioDeviations;
     using Britt2022.A.E.O.InterfacesFactories.Results.SurgeonScenarioDeviations;
--- Britt2022.A.E.O/InterfacesAbstractFactories/IResultsAbstractFactory.cs
+++ Britt2022.A.E.O/InterfacesAbstractFactories/IResultsAbstractFactory.cs
@@ -7,6 +7,7 @@
     using Britt2022.A.E.O.InterfacesFactories.Results.ObjectiveValue;
     using Britt2022.A.E.O.InterfacesFactories.Results.OverallWallTime;
     using Britt2022.A.E.O.InterfacesFactories.Results.ScenarioRecoveryWardCensuses;
+    using Britt2022.A.E.O.InterfacesFactories.Results.ScenarioTotalTimes;
     using Britt2022.A.E.O.InterfacesFactories.Results.SurgeonOperatingRoomDayAssignments;
     using Britt2022.A.E.O.InterfacesFactories.Results.SurgeonOperatingRoomDayScenarioDeviations;
     using Britt2022.A.E.O.InterfacesFactories.Results.SurgeonScenarioDeviations;
