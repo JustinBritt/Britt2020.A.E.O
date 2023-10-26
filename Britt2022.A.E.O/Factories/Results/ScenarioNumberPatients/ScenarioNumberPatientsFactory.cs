1f86eed8d95e5902ea71c4e5694271057dd24b33
Justin Britt <britt@uwindsor.ca>
2023-10-26 10:40:19 -0400

    Added factory

---
+++ Britt2022.A.E.O/Factories/Results/ScenarioNumberPatients/ScenarioNumberPatientsFactory.cs
@@ -0,0 +1,41 @@
+﻿namespace Britt2022.A.E.O.Factories.Results.ScenarioNumberPatients
+{
+    using System;
+    using System.Collections.Immutable;
+
+    using log4net;
+
+    using Britt2022.A.E.O.Classes.Results.ScenarioNumberPatients;
+    using Britt2022.A.E.O.Interfaces.ResultElements.ScenarioNumberPatients;
+    using Britt2022.A.E.O.Interfaces.Results.ScenarioNumberPatients;
+    using Britt2022.A.E.O.InterfacesFactories.Results.ScenarioNumberPatients;
+
+    internal sealed class ScenarioNumberPatientsFactory : IScenarioNumberPatientsFactory
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public ScenarioNumberPatientsFactory()
+        {
+        }
+
+        public IScenarioNumberPatients Create(
+            ImmutableList<IScenarioNumberPatientsResultElement> value)
+        {
+            IScenarioNumberPatients instance = null;
+
+            try
+            {
+                instance = new ScenarioNumberPatients(
+                    value);
+            }
+            catch (Exception exception)
+            {
+                this.Log.Error(
+                    exception.Message,
+                    exception);
+            }
+
+            return instance;
+        }
+    }
+}
---
+++ Britt2022.A.E.O/InterfacesFactories/Results/ScenarioNumberPatients/IScenarioNumberPatientsFactory.cs
@@ -0,0 +1,13 @@
+﻿namespace Britt2022.A.E.O.InterfacesFactories.Results.ScenarioNumberPatients
+{
+    using System.Collections.Immutable;
+
+    using Britt2022.A.E.O.Interfaces.ResultElements.ScenarioNumberPatients;
+    using Britt2022.A.E.O.Interfaces.Results.ScenarioNumberPatients;
+
+    public interface IScenarioNumberPatientsFactory
+    {
+        IScenarioNumberPatients Create(
+            ImmutableList<IScenarioNumberPatientsResultElement> value);
+    }
+}
