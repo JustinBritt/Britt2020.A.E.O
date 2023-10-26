6026860bcc45aab2bed28f015653970139bce119
Justin Britt <britt@uwindsor.ca>
2023-10-26 11:23:03 -0400

    Added factory

---
+++ Britt2022.A.E.O/Factories/Calculations/ScenarioNumberPatients/ScenarioNumberPatientsResultElementCalculationFactory.cs
@@ -0,0 +1,37 @@
+﻿namespace Britt2022.A.E.O.Factories.Calculations.ScenarioNumberPatients
+{
+    using System;
+
+    using log4net;
+
+    using Britt2022.A.E.O.Classes.Calculations.ScenarioNumberPatients;
+    using Britt2022.A.E.O.Interfaces.Calculations.ScenarioNumberPatients;
+    using Britt2022.A.E.O.InterfacesFactories.Calculations.ScenarioNumberPatients;
+
+    internal sealed class ScenarioNumberPatientsResultElementCalculationFactory : IScenarioNumberPatientsResultElementCalculationFactory
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public ScenarioNumberPatientsResultElementCalculationFactory()
+        {
+        }
+
+        public IScenarioNumberPatientsResultElementCalculation Create()
+        {
+            IScenarioNumberPatientsResultElementCalculation instance = null;
+
+            try
+            {
+                instance = new ScenarioNumberPatientsResultElementCalculation();
+            }
+            catch (Exception exception)
+            {
+                this.Log.Error(
+                    exception.Message,
+                    exception);
+            }
+
+            return instance;
+        }
+    }
+}
---
+++ Britt2022.A.E.O/InterfacesFactories/Calculations/ScenarioNumberPatients/IScenarioNumberPatientsResultElementCalculationFactory.cs
@@ -0,0 +1,9 @@
+﻿namespace Britt2022.A.E.O.InterfacesFactories.Calculations.ScenarioNumberPatients
+{
+    using Britt2022.A.E.O.Interfaces.Calculations.ScenarioNumberPatients;
+
+    public interface IScenarioNumberPatientsResultElementCalculationFactory
+    {
+        IScenarioNumberPatientsResultElementCalculation Create();
+    }
+}
