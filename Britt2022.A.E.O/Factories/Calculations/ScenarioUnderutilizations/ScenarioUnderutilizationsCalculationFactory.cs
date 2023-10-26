50f6dbae2993ce9d8655530e594f2cdb826fd3ca
Justin Britt <britt@uwindsor.ca>
2023-10-26 09:52:22 -0400

    Added factory

---
+++ Britt2022.A.E.O/Factories/Calculations/ScenarioUnderutilizations/ScenarioUnderutilizationsCalculationFactory.cs
@@ -0,0 +1,37 @@
+﻿namespace Britt2022.A.E.O.Factories.Calculations.ScenarioUnderutilizations
+{
+    using System;
+
+    using log4net;
+
+    using Britt2022.A.E.O.Classes.Calculations.ScenarioUnderutilizations;
+    using Britt2022.A.E.O.Interfaces.Calculations.ScenarioUnderutilizations;
+    using Britt2022.A.E.O.InterfacesFactories.Calculations.ScenarioUnderutilizations;
+
+    internal sealed class ScenarioUnderutilizationsCalculationFactory : IScenarioUnderutilizationsCalculationFactory
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public ScenarioUnderutilizationsCalculationFactory()
+        {
+        }
+
+        public IScenarioUnderutilizationsCalculation Create()
+        {
+            IScenarioUnderutilizationsCalculation instance = null;
+
+            try
+            {
+                instance = new ScenarioUnderutilizationsCalculation();
+            }
+            catch (Exception exception)
+            {
+                this.Log.Error(
+                    exception.Message,
+                    exception);
+            }
+
+            return instance;
+        }
+    }
+}
