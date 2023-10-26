892ea64634d41e7d0ab985e5872baa1135168c92
Justin Britt <britt@uwindsor.ca>
2023-10-26 09:05:10 -0400

    Added class

---
+++ Britt2022.A.E.O/Classes/ResultElements/ScenarioUnderutilizations/ScenarioUnderutilizationsResultElement.cs
@@ -0,0 +1,25 @@
+﻿namespace Britt2022.A.E.O.Classes.ResultElements.ScenarioUnderutilizations
+{
+    using log4net;
+
+    using Britt2022.A.E.O.Interfaces.IndexElements;
+    using Britt2022.A.E.O.Interfaces.ResultElements.ScenarioUnderutilizations;
+
+    internal sealed class ScenarioUnderutilizationsResultElement : IScenarioUnderutilizationsResultElement
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public ScenarioUnderutilizationsResultElement(
+            IωIndexElement ωIndexElement,
+            decimal value)
+        {
+            this.ωIndexElement = ωIndexElement;
+
+            this.Value = value;
+        }
+
+        public IωIndexElement ωIndexElement { get; }
+
+        public decimal Value { get; }
+    }
+}
---
+++ Britt2022.A.E.O/Interfaces/ResultElements/ScenarioUnderutilizations/IScenarioUnderutilizationsResultElement.cs
@@ -0,0 +1,11 @@
+﻿namespace Britt2022.A.E.O.Interfaces.ResultElements.ScenarioUnderutilizations
+{
+    using Britt2022.A.E.O.Interfaces.IndexElements;
+
+    public interface IScenarioUnderutilizationsResultElement
+    {
+        IωIndexElement ωIndexElement { get; }
+
+        decimal Value { get; }
+    }
+}
