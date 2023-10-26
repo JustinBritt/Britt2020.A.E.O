3a333264fcbd99aea711a95dc5021d2c10d6afd3
Justin Britt <britt@uwindsor.ca>
2023-10-26 10:47:13 -0400

    Added class

---
+++ Britt2022.A.E.O/Classes/ResultElements/SurgeonScenarioNumberPatients/SurgeonScenarioNumberPatientsResultElement.cs
@@ -0,0 +1,30 @@
+﻿namespace Britt2022.A.E.O.Classes.ResultElements.SurgeonScenarioNumberPatients
+{
+    using log4net;
+
+    using Britt2022.A.E.O.Interfaces.IndexElements;
+    using Britt2022.A.E.O.Interfaces.ResultElements.SurgeonScenarioNumberPatients;
+
+    internal sealed class SurgeonScenarioNumberPatientsResultElement : ISurgeonScenarioNumberPatientsResultElement
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public SurgeonScenarioNumberPatientsResultElement(
+            IiIndexElement iIndexElement,
+            IωIndexElement ωIndexElement,
+            int value)
+        {
+            this.iIndexElement = iIndexElement;
+
+            this.ωIndexElement = ωIndexElement;
+
+            this.Value = value;
+        }
+
+        public IiIndexElement iIndexElement { get; }
+
+        public IωIndexElement ωIndexElement { get; }
+
+        public int Value { get; }
+    }
+}
---
+++ Britt2022.A.E.O/Interfaces/ResultElements/SurgeonScenarioNumberPatients/ISurgeonScenarioNumberPatientsResultElement.cs
@@ -0,0 +1,13 @@
+﻿namespace Britt2022.A.E.O.Interfaces.ResultElements.SurgeonScenarioNumberPatients
+{
+    using Britt2022.A.E.O.Interfaces.IndexElements;
+
+    public interface ISurgeonScenarioNumberPatientsResultElement
+    {
+        IiIndexElement iIndexElement { get; }
+
+        IωIndexElement ωIndexElement { get; }
+
+        int Value { get; }
+    }
+}
