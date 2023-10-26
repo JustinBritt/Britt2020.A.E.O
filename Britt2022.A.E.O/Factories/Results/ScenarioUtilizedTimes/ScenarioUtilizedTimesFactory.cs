6a42116de9175974990bee7f4ceb15cfea53a3d6
Justin Britt <britt@uwindsor.ca>
2023-10-26 07:42:13 -0400

    Added factory

---
+++ Britt2022.A.E.O/Factories/Results/ScenarioUtilizedTimes/ScenarioUtilizedTimesFactory.cs
@@ -0,0 +1,41 @@
+﻿namespace Britt2022.A.E.O.Factories.Results.ScenarioUtilizedTimes
+{
+    using System;
+    using System.Collections.Immutable;
+
+    using log4net;
+
+    using Britt2022.A.E.O.Classes.Results.ScenarioUtilizedTimes;
+    using Britt2022.A.E.O.Interfaces.ResultElements.ScenarioUtilizedTimes;
+    using Britt2022.A.E.O.Interfaces.Results.ScenarioUtilizedTimes;
+    using Britt2022.A.E.O.InterfacesFactories.Results.ScenarioUtilizedTimes;
+
+    internal sealed class ScenarioUtilizedTimesFactory : IScenarioUtilizedTimesFactory
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public ScenarioUtilizedTimesFactory()
+        {
+        }
+
+        public IScenarioUtilizedTimes Create(
+            ImmutableList<IScenarioUtilizedTimesResultElement> value)
+        {
+            IScenarioUtilizedTimes instance = null;
+
+            try
+            {
+                instance = new ScenarioUtilizedTimes(
+                    value);
+            }
+            catch (Exception exception)
+            {
+                this.Log.Error(
+                    exception.Message,
+                    exception);
+            }
+
+            return instance;
+        }
+    }
+}
