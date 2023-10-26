570d0f3d447940ad922cf62d443034c43a364692
Justin Britt <britt@uwindsor.ca>
2023-10-26 10:27:10 -0400

    Added class

---
+++ Britt2022.A.E.O/Classes/ResultElements/ScenarioNumberPatients/ScenarioNumberPatientsResultElement.cs
@@ -0,0 +1,25 @@
+﻿namespace Britt2022.A.E.O.Classes.ResultElements.ScenarioNumberPatients
+{
+    using log4net;
+
+    using Britt2022.A.E.O.Interfaces.IndexElements;
+    using Britt2022.A.E.O.Interfaces.ResultElements.ScenarioNumberPatients;
+
+    internal sealed class ScenarioNumberPatientsResultElement : IScenarioNumberPatientsResultElement
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public ScenarioNumberPatientsResultElement(
+            IωIndexElement ωIndexElement,
+            int value)
+        {
+            this.ωIndexElement = ωIndexElement;
+
+            this.Value = value;
+        }
+
+        public IωIndexElement ωIndexElement { get; }
+
+        public int Value { get; }
+    }
+}
---
+++ Britt2022.A.E.O/Interfaces/ResultElements/ScenarioNumberPatients/IScenarioNumberPatientsResultElement.cs
@@ -0,0 +1,11 @@
+﻿namespace Britt2022.A.E.O.Interfaces.ResultElements.ScenarioNumberPatients
+{
+    using Britt2022.A.E.O.Interfaces.IndexElements;
+
+    public interface IScenarioNumberPatientsResultElement
+    {
+        IωIndexElement ωIndexElement { get; }
+
+        int Value { get; }
+    }
+}
