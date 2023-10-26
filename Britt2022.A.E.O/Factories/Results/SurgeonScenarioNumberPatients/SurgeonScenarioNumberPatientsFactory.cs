428f2e661ae9dcb8644c9e4d64c601b58515b4a3
Justin Britt <britt@uwindsor.ca>
2023-10-26 10:59:14 -0400

    Added factory

---
+++ Britt2022.A.E.O/Factories/Results/SurgeonScenarioNumberPatients/SurgeonScenarioNumberPatientsFactory.cs
@@ -0,0 +1,41 @@
+﻿namespace Britt2022.A.E.O.Factories.Results.SurgeonScenarioNumberPatients
+{
+    using System;
+    using System.Collections.Immutable;
+
+    using log4net;
+
+    using Britt2022.A.E.O.Classes.Results.SurgeonScenarioNumberPatients;
+    using Britt2022.A.E.O.Interfaces.ResultElements.SurgeonScenarioNumberPatients;
+    using Britt2022.A.E.O.Interfaces.Results.SurgeonScenarioNumberPatients;
+    using Britt2022.A.E.O.InterfacesFactories.Results.SurgeonScenarioNumberPatients;
+
+    internal sealed class SurgeonScenarioNumberPatientsFactory : ISurgeonScenarioNumberPatientsFactory
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public SurgeonScenarioNumberPatientsFactory()
+        {
+        }
+
+        public ISurgeonScenarioNumberPatients Create(
+            ImmutableList<ISurgeonScenarioNumberPatientsResultElement> value)
+        {
+            ISurgeonScenarioNumberPatients instance = null;
+
+            try
+            {
+                instance = new SurgeonScenarioNumberPatients(
+                    value);
+            }
+            catch (Exception exception)
+            {
+                this.Log.Error(
+                    exception.Message,
+                    exception);
+            }
+
+            return instance;
+        }
+    }
+}
---
+++ Britt2022.A.E.O/InterfacesFactories/Results/SurgeonScenarioNumberPatients/ISurgeonScenarioNumberPatientsFactory.cs
@@ -0,0 +1,13 @@
+﻿namespace Britt2022.A.E.O.InterfacesFactories.Results.SurgeonScenarioNumberPatients
+{
+    using System.Collections.Immutable;
+
+    using Britt2022.A.E.O.Interfaces.ResultElements.SurgeonScenarioNumberPatients;
+    using Britt2022.A.E.O.Interfaces.Results.SurgeonScenarioNumberPatients;
+
+    public interface ISurgeonScenarioNumberPatientsFactory
+    {
+        ISurgeonScenarioNumberPatients Create(
+            ImmutableList<ISurgeonScenarioNumberPatientsResultElement> value);
+    }
+}
