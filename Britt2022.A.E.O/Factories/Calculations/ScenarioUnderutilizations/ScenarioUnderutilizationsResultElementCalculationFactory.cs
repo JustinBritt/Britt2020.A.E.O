a7bcf5d1da419268682cbc8a204874e2314c369c
Justin Britt <britt@uwindsor.ca>
2023-10-26 09:54:20 -0400

    Added factory

---
+++ Britt2022.A.E.O/Factories/Calculations/ScenarioUnderutilizations/ScenarioUnderutilizationsResultElementCalculationFactory.cs
@@ -0,0 +1,37 @@
+﻿namespace Britt2022.A.E.O.Factories.Calculations.ScenarioUnderutilizations
+{
+    using System;
+
+    using log4net;
+
+    using Britt2022.A.E.O.Classes.Calculations.ScenarioUnderutilizations;
+    using Britt2022.A.E.O.Interfaces.Calculations.ScenarioUnderutilizations;
+    using Britt2022.A.E.O.InterfacesFactories.Calculations.ScenarioUnderutilizations;
+
+    internal sealed class ScenarioUnderutilizationsResultElementCalculationFactory : IScenarioUnderutilizationsResultElementCalculationFactory
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public ScenarioUnderutilizationsResultElementCalculationFactory()
+        {
+        }
+
+        public IScenarioUnderutilizationsResultElementCalculation Create()
+        {
+            IScenarioUnderutilizationsResultElementCalculation instance = null;
+
+            try
+            {
+                instance = new ScenarioUnderutilizationsResultElementCalculation();
+            }
+            catch (Exception exception)
+            {
+                this.Log.Error(
+                    exception.Message,
+                    exception);
+            }
+
+            return instance;
+        }
+    }
+}
---
+++ Britt2022.A.E.O/InterfacesFactories/Calculations/ScenarioUnderutilizations/IScenarioUnderutilizationsResultElementCalculationFactory.cs
@@ -0,0 +1,9 @@
+﻿namespace Britt2022.A.E.O.InterfacesFactories.Calculations.ScenarioUnderutilizations
+{
+    using Britt2022.A.E.O.Interfaces.Calculations.ScenarioUnderutilizations;
+
+    public interface IScenarioUnderutilizationsResultElementCalculationFactory
+    {
+        IScenarioUnderutilizationsResultElementCalculation Create();
+    }
+}
