ee4c9f574dc34360c5faa4bffa74b9ef1e653c7a
Justin Britt <britt@uwindsor.ca>
2023-10-26 10:32:39 -0400

    Added factory

---
+++ Britt2022.A.E.O/Factories/ResultElements/ScenarioNumberPatients/ScenarioNumberPatientsResultElementFactory.cs
@@ -0,0 +1,42 @@
+﻿namespace Britt2022.A.E.O.Factories.ResultElements.ScenarioNumberPatients
+{
+    using System;
+
+    using log4net;
+
+    using Britt2022.A.E.O.Classes.ResultElements.ScenarioNumberPatients;
+    using Britt2022.A.E.O.Interfaces.IndexElements;
+    using Britt2022.A.E.O.Interfaces.ResultElements.ScenarioNumberPatients;
+    using Britt2022.A.E.O.InterfacesFactories.ResultElements.ScenarioNumberPatients;
+
+    internal sealed class ScenarioNumberPatientsResultElementFactory : IScenarioNumberPatientsResultElementFactory
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public ScenarioNumberPatientsResultElementFactory()
+        {
+        }
+
+        public IScenarioNumberPatientsResultElement Create(
+            IωIndexElement ωIndexElement,
+            int value)
+        {
+            IScenarioNumberPatientsResultElement instance = null;
+
+            try
+            {
+                instance = new ScenarioNumberPatientsResultElement(
+                    ωIndexElement,
+                    value);
+            }
+            catch (Exception exception)
+            {
+                this.Log.Error(
+                    exception.Message,
+                    exception);
+            }
+
+            return instance;
+        }
+    }
+}
---
+++ Britt2022.A.E.O/InterfacesFactories/ResultElements/ScenarioNumberPatients/IScenarioNumberPatientsResultElementFactory.cs
@@ -0,0 +1,12 @@
+﻿namespace Britt2022.A.E.O.InterfacesFactories.ResultElements.ScenarioNumberPatients
+{
+    using Britt2022.A.E.O.Interfaces.IndexElements;
+    using Britt2022.A.E.O.Interfaces.ResultElements.ScenarioNumberPatients;
+
+    public interface IScenarioNumberPatientsResultElementFactory
+    {
+        IScenarioNumberPatientsResultElement Create(
+            IωIndexElement ωIndexElement,
+            int value);
+    }
+}
