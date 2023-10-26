92a5edbca0a086cb3fee390f5df06a945f54d78e
Justin Britt <britt@uwindsor.ca>
2023-10-26 07:22:11 -0400

    Added factory

---
+++ Britt2022.A.E.O/Factories/ResultElements/ScenarioUtilizedTimes/ScenarioUtilizedTimesResultElementFactory.cs
@@ -0,0 +1,42 @@
+﻿namespace Britt2022.A.E.O.Factories.ResultElements.ScenarioUtilizedTimes
+{
+    using System;
+
+    using log4net;
+
+    using Britt2022.A.E.O.Classes.ResultElements.ScenarioUtilizedTimes;
+    using Britt2022.A.E.O.Interfaces.IndexElements;
+    using Britt2022.A.E.O.Interfaces.ResultElements.ScenarioUtilizedTimes;
+    using Britt2022.A.E.O.InterfacesFactories.ResultElements.ScenarioUtilizedTimes;
+
+    internal sealed class ScenarioUtilizedTimesResultElementFactory : IScenarioUtilizedTimesResultElementFactory
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public ScenarioUtilizedTimesResultElementFactory()
+        {
+        }
+
+        public IScenarioUtilizedTimesResultElement Create(
+            IωIndexElement ωIndexElement,
+            decimal value)
+        {
+            IScenarioUtilizedTimesResultElement instance = null;
+
+            try
+            {
+                instance = new ScenarioUtilizedTimesResultElement(
+                    ωIndexElement,
+                    value);
+            }
+            catch (Exception exception)
+            {
+                this.Log.Error(
+                    exception.Message,
+                    exception);
+            }
+
+            return instance;
+        }
+    }
+}
---
+++ Britt2022.A.E.O/InterfacesFactories/ResultElements/ScenarioUtilizedTimes/IScenarioUtilizedTimesResultElementFactory.cs
@@ -0,0 +1,12 @@
+﻿namespace Britt2022.A.E.O.InterfacesFactories.ResultElements.ScenarioUtilizedTimes
+{
+    using Britt2022.A.E.O.Interfaces.IndexElements;
+    using Britt2022.A.E.O.Interfaces.ResultElements.ScenarioUtilizedTimes;
+
+    public interface IScenarioUtilizedTimesResultElementFactory
+    {
+        IScenarioUtilizedTimesResultElement Create(
+            IωIndexElement ωIndexElement,
+            decimal value);
+    }
+}
