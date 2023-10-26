1065fb38d0abc8324f00944ba8fa804c82ff4cbb
Justin Britt <britt@uwindsor.ca>
2023-10-26 06:43:33 -0400

    Added factory

---
+++ Britt2022.A.E.O/Factories/Results/ScenarioTotalTimes/ScenarioTotalTimesFactory.cs
@@ -0,0 +1,41 @@
+﻿namespace Britt2022.A.E.O.Factories.Results.ScenarioTotalTimes
+{
+    using System;
+    using System.Collections.Immutable;
+
+    using log4net;
+
+    using Britt2022.A.E.O.Classes.Results.ScenarioTotalTimes;
+    using Britt2022.A.E.O.Interfaces.ResultElements.ScenarioTotalTimes;
+    using Britt2022.A.E.O.Interfaces.Results.ScenarioTotalTimes;
+    using Britt2022.A.E.O.InterfacesFactories.Results.ScenarioTotalTimes;
+
+    internal sealed class ScenarioTotalTimesFactory : IScenarioTotalTimesFactory
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public ScenarioTotalTimesFactory()
+        {
+        }
+
+        public IScenarioTotalTimes Create(
+            ImmutableList<IScenarioTotalTimesResultElement> value)
+        {
+            IScenarioTotalTimes instance = null;
+
+            try
+            {
+                instance = new ScenarioTotalTimes(
+                    value);
+            }
+            catch (Exception exception)
+            {
+                this.Log.Error(
+                    exception.Message,
+                    exception);
+            }
+
+            return instance;
+        }
+    }
+}
---
+++ Britt2022.A.E.O/InterfacesFactories/Results/ScenarioTotalTimes/IScenarioTotalTimesFactory.cs
@@ -0,0 +1,13 @@
+﻿namespace Britt2022.A.E.O.InterfacesFactories.Results.ScenarioTotalTimes
+{
+    using System.Collections.Immutable;
+
+    using Britt2022.A.E.O.Interfaces.ResultElements.ScenarioTotalTimes;
+    using Britt2022.A.E.O.Interfaces.Results.ScenarioTotalTimes;
+
+    public interface IScenarioTotalTimesFactory
+    {
+        IScenarioTotalTimes Create(
+            ImmutableList<IScenarioTotalTimesResultElement> value);
+    }
+}
