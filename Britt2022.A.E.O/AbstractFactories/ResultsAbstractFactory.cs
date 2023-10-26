54ce1b2d198e39132ca31e86034d2b93047d3e0d
Justin Britt <britt@uwindsor.ca>
2023-10-26 07:45:55 -0400

    Changes to usings

--- Britt2022.A.E.O/AbstractFactories/ResultsAbstractFactory.cs
+++ Britt2022.A.E.O/AbstractFactories/ResultsAbstractFactory.cs
@@ -12,6 +12,7 @@
     using Britt2022.A.E.O.Factories.Results.OverallWallTime;
     using Britt2022.A.E.O.Factories.Results.ScenarioRecoveryWardCensuses;
     using Britt2022.A.E.O.Factories.Results.ScenarioTotalTimes;
+    using Britt2022.A.E.O.Factories.Results.ScenarioUtilizedTimes;
     using Britt2022.A.E.O.Factories.Results.SurgeonOperatingRoomDayAssignments;
     using Britt2022.A.E.O.Factories.Results.SurgeonOperatingRoomDayScenarioDeviations;
     using Britt2022.A.E.O.Factories.Results.SurgeonScenarioDeviations;
@@ -24,6 +25,7 @@
     using Britt2022.A.E.O.InterfacesFactories.Results.OverallWallTime;
     using Britt2022.A.E.O.InterfacesFactories.Results.ScenarioRecoveryWardCensuses;
     using Britt2022.A.E.O.InterfacesFactories.Results.ScenarioTotalTimes;
+    using Britt2022.A.E.O.InterfacesFactories.Results.ScenarioUtilizedTimes;
     using Britt2022.A.E.O.InterfacesFactories.Results.SurgeonOperatingRoomDayAssignments;
     using Britt2022.A.E.O.InterfacesFactories.Results.SurgeonOperatingRoomDayScenarioDeviations;
     using Britt2022.A.E.O.InterfacesFactories.Results.SurgeonScenarioDeviations;
