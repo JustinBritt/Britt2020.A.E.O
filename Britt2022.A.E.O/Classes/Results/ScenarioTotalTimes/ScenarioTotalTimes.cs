f87f9e13473bfcee1ca89d5910c2cb89b2050a13
Justin Britt <britt@uwindsor.ca>
2023-10-26 06:32:45 -0400

    Added class

---
+++ Britt2022.A.E.O/Classes/Results/ScenarioTotalTimes/ScenarioTotalTimes.cs
@@ -0,0 +1,55 @@
+﻿namespace Britt2022.A.E.O.Classes.Results.ScenarioTotalTimes
+{
+    using System.Collections.Immutable;
+    using System.Linq;
+
+    using log4net;
+
+    using Hl7.Fhir.Model;
+
+    using NGenerics.DataStructures.Trees;
+
+    using Britt2022.A.E.O.Interfaces.IndexElements;
+    using Britt2022.A.E.O.Interfaces.ResultElements.ScenarioTotalTimes;
+    using Britt2022.A.E.O.Interfaces.Results.ScenarioTotalTimes;
+    using Britt2022.A.E.O.InterfacesFactories.Dependencies.Hl7.Fhir.R4.Model;
+
+    internal sealed class ScenarioTotalTimes : IScenarioTotalTimes
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public ScenarioTotalTimes(
+            ImmutableList<IScenarioTotalTimesResultElement> value)
+        {
+            this.Value = value;
+        }
+
+        public ImmutableList<IScenarioTotalTimesResultElement> Value { get; }
+
+        public decimal GetElementAtAsdecimal(
+            IωIndexElement ωIndexElement)
+        {
+            return this.Value
+                .Where(x => x.ωIndexElement == ωIndexElement)
+                .Select(x => x.Value)
+                .SingleOrDefault();
+        }
+
+        public RedBlackTree<INullableValue<int>, INullableValue<decimal>> GetValueForOutputContext(
+            INullableValueFactory nullableValueFactory)
+        {
+            RedBlackTree<INullableValue<int>, INullableValue<decimal>> redBlackTree = new(
+                new Britt2022.A.E.O.Classes.Comparers.NullableValueintComparer());
+
+            foreach (IScenarioTotalTimesResultElement scenarioTotalTimesResultElement in this.Value)
+            {
+                redBlackTree.Add(
+                    scenarioTotalTimesResultElement.ωIndexElement.Value,
+                    nullableValueFactory.Create<decimal>(
+                        scenarioTotalTimesResultElement.Value));
+            }
+
+            return redBlackTree;
+        }
+    }
+}
