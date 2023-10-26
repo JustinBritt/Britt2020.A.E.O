416c6e32b71473bc63df583e9f6152dbe85c3af4
Justin Britt <britt@uwindsor.ca>
2023-10-26 10:49:57 -0400

    Added factory

---
+++ Britt2022.A.E.O/Factories/ResultElements/SurgeonScenarioNumberPatients/SurgeonScenarioNumberPatientsResultElementFactory.cs
@@ -0,0 +1,44 @@
+﻿namespace Britt2022.A.E.O.Factories.ResultElements.SurgeonScenarioNumberPatients
+{
+    using System;
+
+    using log4net;
+
+    using Britt2022.A.E.O.Classes.ResultElements.SurgeonScenarioNumberPatients;
+    using Britt2022.A.E.O.Interfaces.IndexElements;
+    using Britt2022.A.E.O.Interfaces.ResultElements.SurgeonScenarioNumberPatients;
+    using Britt2022.A.E.O.InterfacesFactories.ResultElements.SurgeonScenarioNumberPatients;
+
+    internal sealed class SurgeonScenarioNumberPatientsResultElementFactory : ISurgeonScenarioNumberPatientsResultElementFactory
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public SurgeonScenarioNumberPatientsResultElementFactory()
+        {
+        }
+
+        public ISurgeonScenarioNumberPatientsResultElement Create(
+            IiIndexElement iIndexElement,
+            IωIndexElement ωIndexElement,
+            int value)
+        {
+            ISurgeonScenarioNumberPatientsResultElement resultElement = null;
+
+            try
+            {
+                resultElement = new SurgeonScenarioNumberPatientsResultElement(
+                    iIndexElement,
+                    ωIndexElement,
+                    value);
+            }
+            catch (Exception exception)
+            {
+                this.Log.Error(
+                    exception.Message,
+                    exception);
+            }
+
+            return resultElement;
+        }
+    }
+}
---
+++ Britt2022.A.E.O/InterfacesFactories/ResultElements/SurgeonScenarioNumberPatients/ISurgeonScenarioNumberPatientsResultElementFactory.cs
@@ -0,0 +1,13 @@
+﻿namespace Britt2022.A.E.O.InterfacesFactories.ResultElements.SurgeonScenarioNumberPatients
+{
+    using Britt2022.A.E.O.Interfaces.IndexElements;
+    using Britt2022.A.E.O.Interfaces.ResultElements.SurgeonScenarioNumberPatients;
+
+    public interface ISurgeonScenarioNumberPatientsResultElementFactory
+    {
+        ISurgeonScenarioNumberPatientsResultElement Create(
+            IiIndexElement iIndexElement,
+            IωIndexElement ωIndexElement,
+            int value);
+    }
+}
