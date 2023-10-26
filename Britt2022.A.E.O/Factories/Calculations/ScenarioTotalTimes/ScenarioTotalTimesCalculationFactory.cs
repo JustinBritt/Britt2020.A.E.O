db18620f29ea0d97af58e05766282d115ceb5ca1
Justin Britt <britt@uwindsor.ca>
2023-10-26 09:36:41 -0400

    Added factory

---
+++ Britt2022.A.E.O/Factories/Calculations/ScenarioTotalTimes/ScenarioTotalTimesCalculationFactory.cs
@@ -0,0 +1,37 @@
+﻿namespace Britt2022.A.E.O.Factories.Calculations.ScenarioTotalTimes
+{
+    using System;
+
+    using log4net;
+
+    using Britt2022.A.E.O.Classes.Calculations.ScenarioTotalTimes;
+    using Britt2022.A.E.O.Interfaces.Calculations.ScenarioTotalTimes;
+    using Britt2022.A.E.O.InterfacesFactories.Calculations.ScenarioTotalTimes;
+
+    internal sealed class ScenarioTotalTimesCalculationFactory : IScenarioTotalTimesCalculationFactory
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public ScenarioTotalTimesCalculationFactory()
+        {
+        }
+
+        public IScenarioTotalTimesCalculation Create()
+        {
+            IScenarioTotalTimesCalculation instance = null;
+
+            try
+            {
+                instance = new ScenarioTotalTimesCalculation();
+            }
+            catch (Exception exception)
+            {
+                this.Log.Error(
+                    exception.Message,
+                    exception);
+            }
+
+            return instance;
+        }
+    }
+}
---
+++ Britt2022.A.E.O/InterfacesFactories/Calculations/ScenarioTotalTimes/IScenarioTotalTimesCalculationFactory.cs
@@ -0,0 +1,9 @@
+﻿namespace Britt2022.A.E.O.InterfacesFactories.Calculations.ScenarioTotalTimes
+{
+    using Britt2022.A.E.O.Interfaces.Calculations.ScenarioTotalTimes;
+
+    public interface IScenarioTotalTimesCalculationFactory
+    {
+        IScenarioTotalTimesCalculation Create();
+    }
+}
