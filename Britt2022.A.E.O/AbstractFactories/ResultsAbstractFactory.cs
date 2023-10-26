ac66b209464307c1dc1e0ea4b77647c22e16be52
Justin Britt <britt@uwindsor.ca>
2023-10-26 10:59:44 -0400

    Changes to usings

--- Britt2022.A.E.O/AbstractFactories/ResultsAbstractFactory.cs
+++ Britt2022.A.E.O/AbstractFactories/ResultsAbstractFactory.cs
@@ -19,6 +19,7 @@
     using Britt2022.A.E.O.Factories.Results.SurgeonOperatingRoomDayAssignments;
     using Britt2022.A.E.O.Factories.Results.SurgeonOperatingRoomDayScenarioDeviations;
     using Britt2022.A.E.O.Factories.Results.SurgeonScenarioDeviations;
+    using Britt2022.A.E.O.Factories.Results.SurgeonScenarioNumberPatients;
     using Britt2022.A.E.O.InterfacesAbstractFactories;
     using Britt2022.A.E.O.InterfacesFactories.Results.BestBound;
     using Britt2022.A.E.O.InterfacesFactories.Results.DayScenarioRecoveryWardCensuses;
@@ -35,6 +36,7 @@
     using Britt2022.A.E.O.InterfacesFactories.Results.SurgeonOperatingRoomDayAssignments;
     using Britt2022.A.E.O.InterfacesFactories.Results.SurgeonOperatingRoomDayScenarioDeviations;
     using Britt2022.A.E.O.InterfacesFactories.Results.SurgeonScenarioDeviations;
+    using Britt2022.A.E.O.InterfacesFactories.Results.SurgeonScenarioNumberPatients;
 
     internal sealed class ResultsAbstractFactory : IResultsAbstractFactory
     {
--- Britt2022.A.E.O/InterfacesAbstractFactories/IResultsAbstractFactory.cs
+++ Britt2022.A.E.O/InterfacesAbstractFactories/IResultsAbstractFactory.cs
@@ -15,6 +15,7 @@
     using Britt2022.A.E.O.InterfacesFactories.Results.SurgeonOperatingRoomDayAssignments;
     using Britt2022.A.E.O.InterfacesFactories.Results.SurgeonOperatingRoomDayScenarioDeviations;
     using Britt2022.A.E.O.InterfacesFactories.Results.SurgeonScenarioDeviations;
+    using Britt2022.A.E.O.InterfacesFactories.Results.SurgeonScenarioNumberPatients;
 
     public interface IResultsAbstractFactory
     {
