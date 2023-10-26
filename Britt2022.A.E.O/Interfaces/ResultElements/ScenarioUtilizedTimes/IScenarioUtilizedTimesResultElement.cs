4185ad42826836c1e1a61d372ba1ced373778be0
Justin Britt <britt@uwindsor.ca>
2023-10-26 07:04:05 -0400

    Added class

---
+++ Britt2022.A.E.O/Interfaces/ResultElements/ScenarioUtilizedTimes/IScenarioUtilizedTimesResultElement.cs
@@ -0,0 +1,11 @@
+﻿namespace Britt2022.A.E.O.Interfaces.ResultElements.ScenarioUtilizedTimes
+{
+    using Britt2022.A.E.O.Interfaces.IndexElements;
+
+    public interface IScenarioUtilizedTimesResultElement
+    {
+        IωIndexElement ωIndexElement { get; }
+
+        decimal Value { get; }
+    }
+}
