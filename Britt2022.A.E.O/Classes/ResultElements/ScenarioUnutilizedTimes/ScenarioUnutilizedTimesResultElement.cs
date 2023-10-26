bde5c46595b1226abf0dd70f71745648c0d9939c
Justin Britt <britt@uwindsor.ca>
2023-10-26 08:00:19 -0400

    Added class

---
+++ Britt2022.A.E.O/Classes/ResultElements/ScenarioUnutilizedTimes/ScenarioUnutilizedTimesResultElement.cs
@@ -0,0 +1,25 @@
+﻿namespace Britt2022.A.E.O.Classes.ResultElements.ScenarioUnutilizedTimes
+{
+    using log4net;
+
+    using Britt2022.A.E.O.Interfaces.IndexElements;
+    using Britt2022.A.E.O.Interfaces.ResultElements.ScenarioUnutilizedTimes;
+
+    internal sealed class ScenarioUnutilizedTimesResultElement : IScenarioUnutilizedTimesResultElement
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public ScenarioUnutilizedTimesResultElement(
+            IωIndexElement ωIndexElement,
+            decimal value)
+        {
+            this.ωIndexElement = ωIndexElement;
+
+            this.Value = value;
+        }
+
+        public IωIndexElement ωIndexElement { get; }
+
+        public decimal Value { get; }
+    }
+}
---
+++ Britt2022.A.E.O/Interfaces/ResultElements/ScenarioUnutilizedTimes/IScenarioUnutilizedTimesResultElement.cs
@@ -0,0 +1,11 @@
+﻿namespace Britt2022.A.E.O.Interfaces.ResultElements.ScenarioUnutilizedTimes
+{
+    using Britt2022.A.E.O.Interfaces.IndexElements;
+
+    public interface IScenarioUnutilizedTimesResultElement
+    {
+        IωIndexElement ωIndexElement { get; }
+
+        decimal Value { get; }
+    }
+}
