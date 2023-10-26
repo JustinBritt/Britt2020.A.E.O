a953e7a94185b3e842a926f7e1beb4f50cde9a81
Justin Britt <britt@uwindsor.ca>
2023-10-26 10:55:49 -0400

    Added class

---
+++ Britt2022.A.E.O/Classes/Results/SurgeonScenarioNumberPatients/SurgeonScenarioNumberPatients.cs
@@ -0,0 +1,71 @@
+﻿namespace Britt2022.A.E.O.Classes.Results.SurgeonScenarioNumberPatients
+{
+    using System.Collections.Immutable;
+    using System.Linq;
+
+    using log4net;
+
+    using Hl7.Fhir.Model;
+
+    using NGenerics.DataStructures.Trees;
+
+    using Britt2022.A.E.O.Interfaces.IndexElements;
+    using Britt2022.A.E.O.Interfaces.Indices;
+    using Britt2022.A.E.O.Interfaces.ResultElements.SurgeonScenarioNumberPatients;
+    using Britt2022.A.E.O.Interfaces.Results.SurgeonScenarioNumberPatients;
+    using Britt2022.A.E.O.InterfacesFactories.Dependencies.Hl7.Fhir.R4.Model;
+    
+    internal sealed class SurgeonScenarioNumberPatients : ISurgeonScenarioNumberPatients
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public SurgeonScenarioNumberPatients(
+            ImmutableList<ISurgeonScenarioNumberPatientsResultElement> value)
+        {
+            this.Value = value;
+        }
+
+        public ImmutableList<ISurgeonScenarioNumberPatientsResultElement> Value { get; }
+
+        private int GetElementAtAsint(
+            IiIndexElement iIndexElement,
+            IωIndexElement ωIndexElement)
+        {
+            return this.Value
+                .Where(x => x.iIndexElement == iIndexElement && x.ωIndexElement == ωIndexElement)
+                .Select(x => x.Value)
+                .SingleOrDefault();
+        }
+
+        public RedBlackTree<Organization, RedBlackTree<INullableValue<int>, INullableValue<int>>> GetValueForOutputContext(
+            INullableValueFactory nullableValueFactory,
+            Ii i,
+            Iω ω)
+        {
+            RedBlackTree<Organization, RedBlackTree<INullableValue<int>, INullableValue<int>>> outerRedBlackTree = new(
+                new Britt2022.A.E.O.Classes.Comparers.OrganizationComparer());
+
+            foreach (IiIndexElement iIndexElement in i.Value.Values)
+            {
+                RedBlackTree<INullableValue<int>, INullableValue<int>> innerRedBlackTree = new(
+                    new Britt2022.A.E.O.Classes.Comparers.NullableValueintComparer());
+
+                foreach (IωIndexElement ωIndexElement in ω.Value.Values)
+                {
+                    innerRedBlackTree.Add(
+                        ωIndexElement.Value,
+                        nullableValueFactory.Create<int>(
+                            this.GetElementAtAsint(
+                                iIndexElement,
+                                ωIndexElement)));
+                }
+
+                outerRedBlackTree.Add(
+                    iIndexElement.Value,
+                    innerRedBlackTree);
+            }
+
+            return outerRedBlackTree;
+        }
+    }
+}
---
+++ Britt2022.A.E.O/Interfaces/Results/SurgeonScenarioNumberPatients/ISurgeonScenarioNumberPatients.cs
@@ -0,0 +1,22 @@
+﻿namespace Britt2022.A.E.O.Interfaces.Results.SurgeonScenarioNumberPatients
+{
+    using System.Collections.Immutable;
+
+    using Hl7.Fhir.Model;
+
+    using NGenerics.DataStructures.Trees;
+
+    using Britt2022.A.E.O.Interfaces.Indices;
+    using Britt2022.A.E.O.Interfaces.ResultElements.SurgeonScenarioNumberPatients;
+    using Britt2022.A.E.O.InterfacesFactories.Dependencies.Hl7.Fhir.R4.Model;
+
+    public interface ISurgeonScenarioNumberPatients
+    {
+        ImmutableList<ISurgeonScenarioNumberPatientsResultElement> Value { get; }
+
+        RedBlackTree<Organization, RedBlackTree<INullableValue<int>, INullableValue<int>>> GetValueForOutputContext(
+            INullableValueFactory nullableValueFactory,
+            Ii i,
+            Iω ω);
+    }
+}
