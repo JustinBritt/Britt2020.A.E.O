09fe5f69f2fe4a2e7a8fcf1d616c98909f7cd17c
Justin Britt <britt@uwindsor.ca>
2023-10-26 09:23:21 -0400

    Added factory

---
+++ Britt2022.A.E.O/Factories/Results/ScenarioUnderutilizations/ScenarioUnderutilizationsFactory.cs
@@ -0,0 +1,41 @@
+﻿namespace Britt2022.A.E.O.Factories.Results.ScenarioUnderutilizations
+{
+    using System;
+    using System.Collections.Immutable;
+
+    using log4net;
+
+    using Britt2022.A.E.O.Classes.Results.ScenarioUnderutilizations;
+    using Britt2022.A.E.O.Interfaces.ResultElements.ScenarioUnderutilizations;
+    using Britt2022.A.E.O.Interfaces.Results.ScenarioUnderutilizations;
+    using Britt2022.A.E.O.InterfacesFactories.Results.ScenarioUnderutilizations;
+
+    internal sealed class ScenarioUnderutilizationsFactory : IScenarioUnderutilizationsFactory
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public ScenarioUnderutilizationsFactory()
+        {
+        }
+
+        public IScenarioUnderutilizations Create(
+            ImmutableList<IScenarioUnderutilizationsResultElement> value)
+        {
+            IScenarioUnderutilizations instance = null;
+
+            try
+            {
+                instance = new ScenarioUnderutilizations(
+                    value);
+            }
+            catch (Exception exception)
+            {
+                this.Log.Error(
+                    exception.Message,
+                    exception);
+            }
+
+            return instance;
+        }
+    }
+}
---
+++ Britt2022.A.E.O/InterfacesFactories/Results/ScenarioUnderutilizations/IScenarioUnderutilizationsFactory.cs
@@ -0,0 +1,13 @@
+﻿namespace Britt2022.A.E.O.InterfacesFactories.Results.ScenarioUnderutilizations
+{
+    using System.Collections.Immutable;
+
+    using Britt2022.A.E.O.Interfaces.ResultElements.ScenarioUnderutilizations;
+    using Britt2022.A.E.O.Interfaces.Results.ScenarioUnderutilizations;
+
+    public interface IScenarioUnderutilizationsFactory
+    {
+        IScenarioUnderutilizations Create(
+            ImmutableList<IScenarioUnderutilizationsResultElement> value);
+    }
+}
