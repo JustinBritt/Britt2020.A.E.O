ede62c2646f122086514b84e6324d48d0e2ccfb9
Justin Britt <britt@uwindsor.ca>
2023-10-26 11:29:56 -0400

    Added properties

--- Britt2022.A.E.O/Interfaces/Models/IWGPMModel.cs
+++ Britt2022.A.E.O/Interfaces/Models/IWGPMModel.cs
@@ -4,6 +4,7 @@
 
     using Britt2022.A.E.O.Interfaces.CrossJoins;
     using Britt2022.A.E.O.Interfaces.Indices;
+    using Britt2022.A.E.O.Interfaces.Parameters.Surgeries;
     using Britt2022.A.E.O.Interfaces.Variables;
 
     public interface IWGPMModel
@@ -27,7 +28,11 @@
 
         Iiω iω { get; }
 
+        Ijk jk { get; }
+
         Ikω kω { get; }
+
+        In n { get; }
 
         Id1Minus d1Minus { get; }
 
