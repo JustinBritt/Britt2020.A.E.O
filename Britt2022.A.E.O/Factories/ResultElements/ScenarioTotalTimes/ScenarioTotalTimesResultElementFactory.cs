491e7571cb0a74c990635d70234040b536a64b01
Justin Britt <britt@uwindsor.ca>
2023-10-26 06:37:43 -0400

    Added factory

---
+++ Britt2022.A.E.O/Factories/ResultElements/ScenarioTotalTimes/ScenarioTotalTimesResultElementFactory.cs
@@ -0,0 +1,42 @@
+﻿namespace Britt2022.A.E.O.Factories.ResultElements.ScenarioTotalTimes
+{
+    using System;
+
+    using log4net;
+
+    using Britt2022.A.E.O.Classes.ResultElements.ScenarioTotalTimes;
+    using Britt2022.A.E.O.Interfaces.IndexElements;
+    using Britt2022.A.E.O.Interfaces.ResultElements.ScenarioTotalTimes;
+    using Britt2022.A.E.O.InterfacesFactories.ResultElements.ScenarioTotalTimes;
+
+    internal sealed class ScenarioTotalTimesResultElementFactory : IScenarioTotalTimesResultElementFactory
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public ScenarioTotalTimesResultElementFactory()
+        {
+        }
+
+        public IScenarioTotalTimesResultElement Create(
+            IωIndexElement ωIndexElement,
+            decimal value)
+        {
+            IScenarioTotalTimesResultElement instance = null;
+
+            try
+            {
+                instance = new ScenarioTotalTimesResultElement(
+                    ωIndexElement,
+                    value);
+            }
+            catch (Exception exception)
+            {
+                this.Log.Error(
+                    exception.Message,
+                    exception);
+            }
+
+            return instance;
+        }
+    }
+}
---
+++ Britt2022.A.E.O/InterfacesFactories/ResultElements/ScenarioTotalTimes/IScenarioTotalTimesResultElementFactory.cs
@@ -0,0 +1,12 @@
+﻿namespace Britt2022.A.E.O.InterfacesFactories.ResultElements.ScenarioTotalTimes
+{
+    using Britt2022.A.E.O.Interfaces.IndexElements;
+    using Britt2022.A.E.O.Interfaces.ResultElements.ScenarioTotalTimes;
+
+    public interface IScenarioTotalTimesResultElementFactory
+    {
+        IScenarioTotalTimesResultElement Create(
+            IωIndexElement ωIndexElement,
+            decimal value);
+    }
+}
